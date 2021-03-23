114c0d1f5d6449e6f34a42aa2e74bd20234b3b08
MarinaKarabut <77589480+MarinaKarabut@users.noreply.github.com>
2021-03-23 23:16:50 +0200

    удалила первый вариант в изменении темы

--- src/scripts/changeTheme.js
+++ src/scripts/changeTheme.js
@@ -4,26 +4,6 @@ const Theme = {
     DARK: 'dark-theme',
   };
 
-  // const changeThemeBtn = document.querySelector('.theme-switch__toggle')
-  // const body = document.querySelector('body')
-  // if(!localStorage.theme){
-  //   localStorage.theme = 'light-theme'
-  // }
-  // body.className = localStorage.theme
-  
-  
-
-  // changeThemeBtn.addEventListener('change', onChangeTheme)
- 
-  
-  // function onChangeTheme () {
-  //     body.classList.toggle(Theme.DARK)
-  //     body.classList.contains(Theme.DARK)? 'light-theme' : 'dark-theme'
-  //     localStorage.theme = body.className || 'light-theme'
-
-  // }
-
-
   const changeThemeBtn = document.querySelector('.theme-switch__toggle');
   const body = document.querySelector('body')
   const currentTheme = localStorage.getItem("theme");
