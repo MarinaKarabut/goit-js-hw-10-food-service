194d84a245f8a4baa5343f221bde2f367c08c670
MarinaKarabut <77589480+MarinaKarabut@users.noreply.github.com>
2021-03-23 22:03:20 +0200

    второй вариант изменения темы
    В первом не работает как надо ползунок, во втором варианте исаправила

--- src/scripts/changeTheme.js
+++ src/scripts/changeTheme.js
@@ -4,20 +4,51 @@ const Theme = {
     DARK: 'dark-theme',
   };
 
-  const changeThemeBtn = document.querySelector('.theme-switch__toggle')
-  const body = document.querySelector('body')
-  if(!localStorage.theme){
-    localStorage.theme = 'light-theme'
-  }
-  body.className = localStorage.theme
+  // const changeThemeBtn = document.querySelector('.theme-switch__toggle')
+  // const body = document.querySelector('body')
+  // if(!localStorage.theme){
+  //   localStorage.theme = 'light-theme'
+  // }
+  // body.className = localStorage.theme
+  
+  
 
-  changeThemeBtn.addEventListener('change', onChangeTheme)
+  // changeThemeBtn.addEventListener('change', onChangeTheme)
  
   
-  function onChangeTheme () {
-      body.classList.toggle(Theme.DARK)
-      body.classList.contains(Theme.DARK)? 'light-theme' : 'dark-theme'
-      localStorage.theme = body.className || 'light-theme'
+  // function onChangeTheme () {
+  //     body.classList.toggle(Theme.DARK)
+  //     body.classList.contains(Theme.DARK)? 'light-theme' : 'dark-theme'
+  //     localStorage.theme = body.className || 'light-theme'
 
+  // }
+
+
+  const changeThemeBtn = document.querySelector('.theme-switch__toggle');
+  const body = document.querySelector('body')
+  const currentTheme = localStorage.getItem("theme");
+  
+  
+  if (currentTheme) {
+    body.classList.add(currentTheme);
+  
+    if (currentTheme === Theme.DARK) {
+      changeThemeBtn.checked = true;
+    }
   }
+
+  function onChangeTheme(e) {
+    if (e.target.checked) {
+      localStorage.setItem("theme", Theme.DARK);
+      body.classList.toggle(Theme.DARK);
+      body.classList.remove(Theme.LIGHT);
+
+    } else {
+      localStorage.setItem("theme", Theme.LIGHT);
+      body.classList.toggle(Theme.LIGHT);
+      body.classList.remove(Theme.DARK);
       
+    }
+  }
+
+  changeThemeBtn.addEventListener("change", onChangeTheme);
