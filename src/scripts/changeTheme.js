7be1042bb1ae8b0312a6c354f8c3d8c8425b8a03
MarinaKarabut <77589480+MarinaKarabut@users.noreply.github.com>
2021-03-24 11:50:38 +0200

    изменения кода

--- src/scripts/changeTheme.js
+++ src/scripts/changeTheme.js
@@ -6,29 +6,19 @@ const Theme = {
 
   const changeThemeBtn = document.querySelector('.theme-switch__toggle');
   const body = document.querySelector('body')
-  const currentTheme = localStorage.getItem("theme");
+  body.classList.add(Theme.LIGHT)
   
-  
-  if (currentTheme) {
-    body.classList.add(currentTheme);
-  
-    if (currentTheme === Theme.DARK) {
+  changeThemeBtn.addEventListener("change", onChangeTheme);
+
+  if (localStorage.getItem("theme") === 'true') {
+    body.classList.replace(Theme.LIGHT, Theme.DARK);
       changeThemeBtn.checked = true;
     }
-  }
+
 
   function onChangeTheme(e) {
-    if (e.target.checked) {
-      localStorage.setItem("theme", Theme.DARK);
-      body.classList.toggle(Theme.DARK);
-      body.classList.remove(Theme.LIGHT);
-
-    } else {
-      localStorage.setItem("theme", Theme.LIGHT);
-      body.classList.toggle(Theme.LIGHT);
-      body.classList.remove(Theme.DARK);
-      
-    }
+    const switchTheme = body.classList.toggle(Theme.DARK);
+      localStorage.setItem("theme", switchTheme);
   }
 
-  changeThemeBtn.addEventListener("change", onChangeTheme);
+ 
