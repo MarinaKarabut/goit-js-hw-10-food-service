6c0a02871479bb4c9d44a3f5eab8e653778f0a14
MarinaKarabut <77589480+MarinaKarabut@users.noreply.github.com>
2021-03-24 13:39:02 +0200

    добавила второй вариант изменения темы

--- src/scripts/changeTheme.js
+++ src/scripts/changeTheme.js
@@ -21,4 +21,31 @@ const Theme = {
       localStorage.setItem("theme", switchTheme);
   }
 
- 
+
+//   const changeThemeBtn = document.querySelector('.theme-switch__toggle');
+//   const body = document.querySelector('body')
+//   const currentTheme = localStorage.getItem("theme");
+
+// if (currentTheme) {
+//   body.classList.add(currentTheme);
+
+//   if (currentTheme === Theme.DARK) {
+//     changeThemeBtn.checked = true;
+//   }
+// }
+
+// function onChangeTheme(e) {
+//   if (e.target.checked) {
+//     localStorage.setItem("theme", Theme.DARK);
+//     body.classList.toggle(Theme.DARK);
+//     body.classList.remove(Theme.LIGHT);
+    
+//   } else {
+//     localStorage.setItem("theme", Theme.LIGHT);
+//     body.classList.toggle(Theme.LIGHT);
+//     body.classList.remove(Theme.DARK);
+//   }
+// }
+
+
+// changeThemeBtn.addEventListener("change", onChangeTheme);
